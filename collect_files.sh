27fe60678ded1ef263dfae9486dba00e5636365f
vvvv6v <vshivtsevvlad@yandex.ru>
2025-04-28 23:46:02 +0300

    Update collect_files.sh

--- collect_files.sh
+++ collect_files.sh
@@ -1,35 +1,36 @@
 #!/usr/bin/env bash
 set -euo pipefail
-mdirs=-1
-if [[ $# -ge 3 && $1 == --max_depth ]]; then
-  m=$2
-  if ! [[ $m =~ ^[0-9]+$ && $m -ge 1 ]]; then exit 1; fi
-  mdirs=$((m-1))
-  shift 2
-fi
-[[ $# -eq 2 ]] || exit 1
-in=$1; out=$2
-mkdir -p "$out"
-find "$in" -type f -print0 | while IFS= read -r -d '' f; do
-  rel=${f#"$in"/}
-  IFS=/ read -ra p <<< "$rel"
-  len=${#p[@]}; fn=${p[len-1]}; dc=$((len-1))
-  if (( mdirs>=0 )); then
-    if (( dc<=mdirs )); then kd=$dc; si=0; else kd=$mdirs; si=$((dc-kd)); fi
+md=1
+declare -a pos
+while (($#)); do
+  case $1 in
+    --max_depth) md=$2; shift 2 ;;
+    *) pos+=("$1"); shift ;;
+  esac
+done
+src=${pos[0]}; dst=${pos[1]}
+mkdir -p "$dst"
+find "$src" -type f -print0 | while IFS= read -r -d '' f; do
+  r=${f#"$src"/}
+  IFS=/ read -ra a <<< "$r"
+  n=${#a[@]}
+  if (( md>0 && n>md )); then
+    s=$((n-md))
+    b=("${a[@]:s:md}")
   else
-    kd=$dc; si=0
+    b=("${a[@]}")
   fi
-  if (( kd>0 )); then
-    sd=( "${p[@]:si:kd}" )
-    sub=$(IFS=/; echo "${sd[*]}")
-    dd="$out/$sub"
-  else
-    dd="$out"
-  fi
-  mkdir -p "$dd"
-  bn=${fn%.*}; ex=${fn##*.}
-  [[ $bn == $fn ]] && e="" || e=".$ex"
-  dest="$dd/$bn$e"; i=1
-  while [[ -e $dest ]]; do dest="$dd/${bn}${i}${e}"; ((i++)); done
-  cp "$f" "$dest"
+  pp=$(IFS=/; echo "${b[*]}")
+  dir=$(dirname "$pp")
+  td="$dst/$dir"
+  mkdir -p "$td"
+  name=${b[-1]}
+  bn=${name%.*}; ex=${name##*.}
+  [[ $bn == $name ]] && ext="" || ext=".$ex"
+  out="$td/$bn$ext"; i=1
+  while [[ -e $out ]]; do
+    out="$td/${bn}${i}${ext}"
+    ((i++))
+  done
+  cp "$f" "$out"
 done
